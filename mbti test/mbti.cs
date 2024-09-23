b9291bb1f169c017d5f9e960f1edd9036936d694
Tsabitah <163952763+tsabitahh@users.noreply.github.com>
2024-09-23 09:31:00 +0700

    initial commit

---
+++ mbti test/mbti.cs
@@ -0,0 +1,60 @@
+﻿class Mbti {
+  public static void Main (string[] args) {
+
+    // questions 
+    string[] quiz = {
+      "1. I prefer to be alone \n2. I gain energy from social gathering",
+      "3. I'm more imaginative \n4. I trust hard facts",
+      "5. I need to include others' when making decision \n6. I like to make decision based on logical thinking",
+      "7. I'm flexible and adaptable \n8. I like to make plans and follow through"
+    };
+
+    // assigned asnwers
+    string r = "Introvert";
+    string e = "Extrovert";
+    string v = "Intuitive";
+    string s = "Sensing";
+    string f = "Feeling";
+    string t = "Thinking";
+    string p = "Perceiving";
+    string j = "Judging";
+
+    // questions displayed
+    Console.WriteLine("MBTI Quick Personality Test");
+    for (int i=0; i < quiz.Length; i++) {
+      Console.WriteLine("Question");
+      Console.WriteLine(quiz[i]);
+      Console.Write("Please enter your answer ");
+      string answer = Console.ReadLine();
+
+      // verify each answer
+      if (string.Equals(answer, "1", StringComparison.OrdinalIgnoreCase)) {
+        Console.WriteLine(r);
+      }
+      else if (string.Equals(answer, "2", StringComparison.OrdinalIgnoreCase)) {
+        Console.WriteLine(e);
+      }
+      else if (string.Equals(answer, "3", StringComparison.OrdinalIgnoreCase)) {
+        Console.WriteLine(v);
+      }
+      else if (string.Equals(answer, "4", StringComparison.OrdinalIgnoreCase)) {
+        Console.WriteLine(s);
+      }
+      else if (string.Equals(answer, "5", StringComparison.OrdinalIgnoreCase)) {
+        Console.WriteLine(f);
+      }
+      else if (string.Equals(answer, "6", StringComparison.OrdinalIgnoreCase)) {
+        Console.WriteLine(t);
+      }
+      else if (string.Equals(answer, "7", StringComparison.OrdinalIgnoreCase)) {
+        Console.WriteLine(p);
+      }
+      else if (string.Equals(answer, "8", StringComparison.OrdinalIgnoreCase)) {
+        Console.WriteLine(j);
+      } 
+    }
+
+    Console.WriteLine("MBTI Personality Test Completed!");
+
+  }
+}
---
+++ uv index status/uvstats.cs
@@ -0,0 +1,24 @@
+class UvStats {
+  public static void Main (string[] args) {
+    // input UV Index from 1-11
+    Console.WriteLine("Enter a UV index:");
+    int uvIndex = Convert.ToInt32(Console.ReadLine());
+
+    // each of the UV Index output
+    if (uvIndex <= 2) {
+      Console.WriteLine("Low UV. No protections needed");
+      }
+
+    else if (uvIndex >= 3 && uvIndex <= 5) {
+    Console.WriteLine("Moderate UV. Good for tanning");
+    }
+
+    else if (uvIndex >=6 && uvIndex <= 7) {
+      Console.WriteLine("High UV. Wear sunscreen and other protections");
+    }
+
+    else if (uvIndex >= 8 && uvIndex <= 10) {
+      Console.WriteLine("Very High UV. This UV could potentially cause skin cancer");
+      }
+  }
+}
