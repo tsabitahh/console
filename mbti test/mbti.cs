a5a327112da36a506a69fb1d6be3af4fc738acc0
Tsabitah <163952763+tsabitahh@users.noreply.github.com>
2025-01-17 08:24:26 +0700

    Clean code

--- mbti test/mbti.cs
+++ mbti test/mbti.cs
@@ -28,33 +28,15 @@
       string answer = Console.ReadLine();
 
       // verify each answer
-      if (string.Equals(answer, "1", StringComparison.OrdinalIgnoreCase)) {
-        Console.WriteLine(r);
-      }
-      else if (string.Equals(answer, "2", StringComparison.OrdinalIgnoreCase)) {
-        Console.WriteLine(e);
-      }
-      else if (string.Equals(answer, "3", StringComparison.OrdinalIgnoreCase)) {
-        Console.WriteLine(v);
-      }
-      else if (string.Equals(answer, "4", StringComparison.OrdinalIgnoreCase)) {
-        Console.WriteLine(s);
-      }
-      else if (string.Equals(answer, "5", StringComparison.OrdinalIgnoreCase)) {
-        Console.WriteLine(f);
-      }
-      else if (string.Equals(answer, "6", StringComparison.OrdinalIgnoreCase)) {
-        Console.WriteLine(t);
-      }
-      else if (string.Equals(answer, "7", StringComparison.OrdinalIgnoreCase)) {
-        Console.WriteLine(p);
-      }
-      else if (string.Equals(answer, "8", StringComparison.OrdinalIgnoreCase)) {
-        Console.WriteLine(j);
-      } 
+      if (string.Equals(answer, "1", StringComparison.OrdinalIgnoreCase)) {Console.WriteLine(r);}
+      else if (string.Equals(answer, "2", StringComparison.OrdinalIgnoreCase)) {Console.WriteLine(e);}
+      else if (string.Equals(answer, "3", StringComparison.OrdinalIgnoreCase)) {Console.WriteLine(v);}
+      else if (string.Equals(answer, "4", StringComparison.OrdinalIgnoreCase)) {Console.WriteLine(s);}
+      else if (string.Equals(answer, "5", StringComparison.OrdinalIgnoreCase)) {Console.WriteLine(f);}
+      else if (string.Equals(answer, "6", StringComparison.OrdinalIgnoreCase)) {Console.WriteLine(t);}
+      else if (string.Equals(answer, "7", StringComparison.OrdinalIgnoreCase)) {Console.WriteLine(p);}
+      else if (string.Equals(answer, "8", StringComparison.OrdinalIgnoreCase)) {Console.WriteLine(j);} 
     }
-
     Console.WriteLine("MBTI Personality Test Completed!");
-
   }
 }
